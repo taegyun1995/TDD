92408ed7e8d84df0dfd14c645c9f02eb360e66ce
taegyun <needle12s@gmail.com>
2026-01-08 23:58:16 +0900

    TDD Kata: 문자열 Calculator 중복 제거 - Calculator 인스턴스를 매번 생성

--- src/test/java/com/taegyun/tdd/string_calculator/CalculatorTest.java
+++ src/test/java/com/taegyun/tdd/string_calculator/CalculatorTest.java
@@ -6,46 +6,36 @@ import org.assertj.core.api.Assertions;
 
 class CalculatorTest {
 
+    Calculator calculator = new Calculator();
+
     @Test
     void 빈_문자열_또는_null이면_0을_반환한다() {
-        Calculator calculator = new Calculator();
-
         Assertions.assertThat(calculator.add(null)).isEqualTo(0);
         Assertions.assertThat(calculator.add("")).isEqualTo(0);
     }
 
     @Test
     void 숫자_하나면_해당_숫자를_반환한다() {
-        Calculator calculator = new Calculator();
-
         Assertions.assertThat(calculator.add("7")).isEqualTo(7);
     }
 
     @Test
     void 쉼표_구분자로_숫자의_합을_반환한다() {
-        Calculator calculator = new Calculator();
-
         Assertions.assertThat(calculator.add("1,2,3")).isEqualTo(6);
     }
 
     @Test
     void 공백을_포함한_쉼표_구분자로_숫자의_합을_반환한다() {
-        Calculator calculator = new Calculator();
-
         Assertions.assertThat(calculator.add("1, 2, 3")).isEqualTo(6);
     }
 
     @Test
     void 쉼표_외에_줄바꿈_구분자도_숫자의_합을_반환한다() {
-        Calculator calculator = new Calculator();
-
         Assertions.assertThat(calculator.add("1, 2\n 3")).isEqualTo(6);
     }
 
     @Test
     void 커스텀_구분자를_사용하여_숫자의_합을_반환한다() {
-        Calculator calculator = new Calculator();
-
         Assertions.assertThat(calculator.add("//;\n1;2;3")).isEqualTo(6);
     }
 }
